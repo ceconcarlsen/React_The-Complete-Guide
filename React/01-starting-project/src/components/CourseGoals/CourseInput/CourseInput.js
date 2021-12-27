ff499a41d91845432b6119143688ca2d82d1fafd
ceconcarlsen <ceconcarlsen@gmail.com>
2021-12-27 16:18:31 -0300

    Debugging React Apps

--- React/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ React/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,38 +1,10 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
 
-import Button from "../../UI/Button/Button";
-import styles from "./CourseInput.module.css";
-
-// const FormControl = styled.div`
-//   margin: 0.5rem 0;
-
-//   & label {
-//     font-weight: bold;
-//     display: block;
-//     margin-bottom: 0.5rem;
-//     color: ${(props) => (props.invalid ? "red" : "black")};
-//   }
-
-//   & input {
-//     display: block;
-//     width: 100%;
-//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")}; //estilização dinâmica
-//     background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
-//     font: inherit;
-//     line-height: 1.5rem;
-//     padding: 0 0.25rem;
-//   }
-
-//   & input:focus {
-//     outline: none;
-//     background: #fad0ec;
-//     border-color: #8b005d;
-//   }
-// `;
+import Button from '../../UI/Button/Button';
+import styles from './CourseInput.module.css';
 
 const CourseInput = (props) => {
-  const [enteredValue, setEnteredValue] = useState("");
+  const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
@@ -45,7 +17,6 @@ const CourseInput = (props) => {
   const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
-      //entrada vazia
       setIsValid(false);
       return;
     }
@@ -55,7 +26,7 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div
-        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
